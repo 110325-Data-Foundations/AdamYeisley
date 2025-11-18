9460d7bd8200cb0d175ffff558468168e0666145
Adam Yeisley <ovo.adam.yeisley.ovo@gmail.com>
2025-11-18 14:57:08 -0600

    SQL Notes

--- SQL/SQL_Challenges.sql
+++ SQL/SQL_Challenges.sql
@@ -1,1 +1,113 @@
+-- BASIC CHALLENGES
+
+-- List all customers (full name, customer id, and country) who are not in the USA
+SELECT first_name, last_name, customer_id, country FROM customer WHERE country <> 'USA';
+
+-- List all customers from Brazil
+SELECT * FROM customer WHERE country = 'Brazil';
+
+-- List all sales agents
+SELECT * FROM employee WHERE title = 'Sales Support Agent';
+
+-- SELECT * FROM employee WHERE title LIKE '%Agent%;
+SELECT * FROM employee WHERE title LIKE '%Agent';
+
+-- Retrieve a list of all countries in billing addresses on invoices
+SELECT billing_country FROM invoice;
+
+-- Retrieve how many invoices there were in 2009, and what was the sales total for that year?
 SELECT 
+COUNT(*) AS invoice_count,
+SUM(total) AS sales_total
+FROM invoice
+WHERE invoice_date >= '2009-01-01' AND invoice_date < '2010-01-01';
+
+-- (challenge: find the invoice count sales total for every year using one query)
+
+
+-- how many line items were there for invoice #37
+SELECT COUNT(*) AS line_item_count
+FROM invoice_line WHERE invoice_id = 37;
+
+-- how many invoices per country? BillingCountry  # of invoices 
+SELECT COUNT(*) AS invoices_per_country 
+FROM 
+
+
+-- Retrieve the total sales per country, ordered by the highest total sales first.
+
+-- JOINS CHALLENGES
+-- Every Album by Artist
+
+
+-- (inner keyword is optional for inner join)
+
+-- All songs of the rock genre
+
+
+-- Show all invoices of customers from brazil (mailing address not billing)
+
+
+-- Show all invoices together with the name of the sales agent for each one
+
+
+-- Which sales agent made the most sales in 2009?
+
+
+-- How many customers are assigned to each sales agent?
+
+
+-- Which track was purchased the most in 2010?
+
+
+-- Show the top three best selling artists.
+
+
+-- Which customers have the same initials as at least one other customer?
+
+
+-- Which countries have the most invoices?
+
+
+-- Which city has the customer with the highest sales total?
+
+
+-- Who is the highest spending customer?
+
+
+-- Return the email and full name of of all customers who listen to Rock.
+
+
+-- Which artist has written the most Rock songs?
+
+
+-- Which artist has generated the most revenue?
+
+
+
+
+-- ADVANCED CHALLENGES
+-- solve these with a mixture of joins, subqueries, CTE, and set operators.
+-- solve at least one of them in two different ways, and see if the execution
+-- plan for them is the same, or different.
+
+-- 1. which artists did not make any albums at all?
+
+
+-- 2. which artists did not record any tracks of the Latin genre?
+
+
+-- 3. which video track has the longest length? (use media type table)
+
+
+
+-- 4. boss employee (the one who reports to nobody)
+
+
+-- 5. how many audio tracks were bought by German customers, and what was
+--    the total price paid for them?
+
+
+
+-- 6. list the names and countries of the customers supported by an employee
+--    who was hired younger than 35.
--- SQL/SQL_NOTES.sql
+++ SQL/SQL_NOTES.sql
@@ -89,3 +89,47 @@ GROUP BY genre_id
 HAVING count(*) > 100                     -- HAVING is like WHERE, but for grouped results
 ORDER BY track_count DESC;
 
+-- Joins and Subqueries
+
+/*
+Returning data across multiple tables
+You could run individual selescts and cross refrence manually, but that's not efficent
+and we're not returning everything as a descrete set of data
+*/
+
+-- I want to return the album title and artist name of every album
+
+-- Inner join (default when using just JOIN) - only records that match are returned
+SELECT a.title AS album, ar.name AS artist
+FROM album AS a
+INNER JOIN artist ar ON a.artist_id = ar.artist_id;
+
+-- Left and Right Joins
+-- These are mirrors of each other, your left table is the first table you select (next to FROM)
+-- The right table is the second one selected (after the JOIN)
+
+-- LEFT JOIN - All record from the left table + matches from the right
+SELECT ar.name, a.title
+FROM artist ar
+LEFT JOIN album a on ar.artist_id = a.artist_id
+WHERE ar.name LIKE 'A%';
+
+-- RIGHT JOIN - Same as left, but right table + matches from the left
+
+-- OUTER JOIN
+
+-- FULL OUTER JOIN returns all rows from both tables, matches records, 
+-- shows null for no match, essentially a complete union of both tables
+
+SELECT ar.name, a.title
+FROM artist ar
+FULL OUTER JOIN album a on ar.artist_id = a.artist_id
+
+-- CROSS JOIN - Cartesian Product
+SELECT g.name as genre, mt.name as media_type
+FROM genre g
+CROSS JOIN media_type mt
+WHERE g.name IN ('Rock','Jazz')
+
+SELECT * FROM genre;
+
